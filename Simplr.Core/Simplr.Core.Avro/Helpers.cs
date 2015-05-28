fa2e81848bea1d6ed250e4ead952bfcbab86147b
Dovydas Navickas <dovydas.nav@gmail.com>
2015-05-28 23:46:05 +0300

    Simplr.Core.Avro resolver project added.

---
+++ Simplr.Core/Simplr.Core.Avro/Helpers.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace Simplr.Core.Avro
+{
+    public static class Helpers
+    {
+        public static bool CanContainNull(this Type type)
+        {
+            var underlyingType = Nullable.GetUnderlyingType(type);
+            return !type.IsValueType || underlyingType != null;
+        }
+        public static IEnumerable<FieldInfo> GetAllFields(this Type t)
+        {
+            if (t == null)
+            {
+                return Enumerable.Empty<FieldInfo>();
+            }
+
+            const BindingFlags Flags =
+                BindingFlags.Public |
+                BindingFlags.NonPublic |
+                BindingFlags.Instance |
+                BindingFlags.DeclaredOnly;
+            return t
+                .GetFields(Flags)
+                .Where(f => !f.IsDefined(typeof(CompilerGeneratedAttribute), false))
+                .Concat(GetAllFields(t.BaseType));
+        }
+
+        public static IEnumerable<PropertyInfo> GetAllProperties(this Type t)
+        {
+            if (t == null)
+            {
+                return Enumerable.Empty<PropertyInfo>();
+            }
+
+            const BindingFlags Flags =
+                BindingFlags.Public |
+                BindingFlags.NonPublic |
+                BindingFlags.Instance |
+                BindingFlags.DeclaredOnly;
+
+            return t
+                .GetProperties(Flags)
+                .Where(p => !p.IsDefined(typeof(CompilerGeneratedAttribute), false)
+                            && p.GetIndexParameters().Length == 0)
+                .Concat(GetAllProperties(t.BaseType));
+        }
+
+        public static IList<PropertyInfo> RemoveDuplicates(IEnumerable<PropertyInfo> properties)
+        {
+            var result = new List<PropertyInfo>();
+            foreach (var p in properties)
+            {
+                if (result.Find(s => s.Name == p.Name) == null)
+                {
+                    result.Add(p);
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Simplr.Core/Simplr.Core.Avro/SimplrAvroResolver.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Hadoop.Avro;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace Simplr.Core.Avro
+{
+    public class SimplrAvroResolver : AvroPublicMemberContractResolver
+    {
+        private bool AllowNullable;
+        private Type[] knownTypes { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AvroPublicMemberContractResolver"/> class.
+        /// </summary>
+        public SimplrAvroResolver() : base(false)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AvroPublicMemberContractResolver"/> class.
+        /// </summary>
+        /// <param name="allowNullable">If set to <c>true</c>, null values are allowed.</param>
+        public SimplrAvroResolver(bool allowNullable)
+            : base(allowNullable)
+        {
+            AllowNullable = allowNullable;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AvroPublicMemberContractResolver"/> class.
+        /// </summary>
+        /// <param name="allowNullable">If set to <c>true</c>, null values are allowed.</param>
+        /// <param name="knownTypes">If types list provided and not empty, adds them to all known types</param>
+        public SimplrAvroResolver(bool allowNullable, Type[] knownTypes)
+            : base(allowNullable)
+        {
+            this.knownTypes = knownTypes;
+        }
+        public override IEnumerable<Type> GetKnownTypes(Type type)
+        {
+            if (type == null)
+            {
+                throw new ArgumentNullException("type");
+            }
+            return knownTypes;
+        }
+        public override TypeSerializationInfo ResolveType(Type type)
+        {
+            try
+            {
+                return base.ResolveType(type);
+            }
+            catch (SerializationException)
+            {
+                if (type == typeof(object) && knownTypes != null && knownTypes.Length != 0)
+                {
+                    return new TypeSerializationInfo
+                    {
+                        Name = StripAvroNonCompatibleCharacters(type.Name),
+                        Namespace = StripAvroNonCompatibleCharacters(type.Namespace),
+                        Nullable = AllowNullable && type.CanContainNull()
+                    };
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+    }
+}
