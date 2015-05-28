852622b9a78ffa62467d34bb0551dff5344f3c76
Dovydas Navickas <dovydas.nav@gmail.com>
2015-05-28 23:46:52 +0300

    Simplr.Core.WebJobs project with NinjectJobActivator

---
+++ Simplr.Core/Simplr.Core.WebJobs/NinjectJobActivator.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Azure.WebJobs.Host;
+using Ninject;
+
+namespace Simplr.Core.WebJobs
+{
+    public class NinjectJobActivator : IJobActivator
+    {
+        public IKernel Kernel { get; private set; }
+
+        public NinjectJobActivator(IKernel kernel)
+        {
+            Kernel = kernel;
+        }
+
+        public T CreateInstance<T>()
+        {
+            return Kernel.Get<T>();
+        }
+    }
+}
