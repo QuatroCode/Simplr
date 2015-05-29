bc76043d5b8f0db409f069ba448ce35012e53d08
Dovydas Navickas <dovydas.nav@gmail.com>
2015-05-29 15:50:58 +0300

    IdentityExtensions added to Core.Web

---
+++ Simplr.Core/Simplr.Core.Web/Extensions/IdentityExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Identity;
+using System.Security.Authentication;
+using System.Security.Principal;
+
+namespace Simplr.Core.Web.Extensions
+{
+    public static class IdentityExtensions
+    {
+        public static string GetUserId(this IPrincipal user)
+        {
+            if (user?.Identity == null)
+            {
+                throw new AuthenticationException();
+            }
+            return user.Identity.GetUserId();
+        }
+    }
+}
